a751c52ff0d98da4fa240205218dabea32db8c21
ariframaps <ariframa@student.ub.ac.id>
2024-05-04 17:15:18 +0700

    blog preview card and homepage interface

--- src/components/BlogPreviewCard.jsx
+++ src/components/BlogPreviewCard.jsx
@@ -1,5 +1,22 @@
-export const BlogPreviewCard = () => {
+export const BlogPreviewCard = ({ post }) => {
     return (
-        <div>BlogPreviewCard</div>
+        <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+            <a href="#">
+                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{post.title}1</h5>
+            </a>
+            <p className="max-w-screen-lg mb-5 font-normal text-gray-700 dark:text-gray-400">{post.content}</p>
+            <div className="flex w-full justify-between">
+                <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                    Read more
+                </a>
+                <span>
+                    <span className="text-gray-400">Author :</span>
+                    <a href="#" className="ms-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-400 hover:bg-gray-700 dark:bg-gray-600 dark:hover:bg-gray-700">
+                        {post.author}
+                    </a>
+                </span>
+            </div>
+        </div>
+
     )
 }
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,5 +1,34 @@
+import { BlogPreviewCard } from "../../components/BlogPreviewCard"
+
 export const HomePage = () => {
+    const blogPosts = [
+        {
+            title: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+            content: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor incididunt ut labore et dolore mag at vero eos et dolore mag et dolore mag et',
+            author: 'Arif Rama',
+            id: 1
+        },
+        {
+            title: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+            content: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor incididunt ut labore et dolore mag at vero eos et dolore mag et dolore mag et',
+            author: 'Putra',
+            id: 2
+        },
+        {
+            title: 'lorem ipsum dolor sit amet, consectetur adipiscing',
+            content: 'lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonumy eirmod tempor incididunt ut labore et dolore mag at vero eos et dolore mag et dolore mag et',
+            author: 'Itachi',
+            id: 3
+        },
+    ]
+
     return (
-        <main>HomePage</main>
+        <main className="py-10">
+            <ul className="max-w-screen-xl flex flex-col gap-5 m-auto">
+                {blogPosts && blogPosts.map(post => (
+                    <BlogPreviewCard key={post.id} post={post} />
+                ))}
+            </ul>
+        </main>
     )
 }
