3f4aac6fd66632e246d7982e939ae03ec3d41ff5
ariframaps <ariframa@student.ub.ac.id>
2024-05-08 10:21:53 +0700

    edit button fix move on the blog page

--- src/components/BlogPreviewCard.jsx
+++ src/components/BlogPreviewCard.jsx
@@ -19,10 +19,6 @@ export const BlogPreviewCard = ({ post, authors, toggle, setToggle }) => {
         })
     }
 
-    function handleEdit() {
-        console.log('edit post')
-    }
-
     return (
         <div className="w-full p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
             <Link to={`/blog/${post.id}`}>
--- src/pages/BLogPage.jsx
+++ src/pages/BLogPage.jsx
@@ -1,14 +1,12 @@
 import { useEffect, useState } from "react"
 import { useParams, Link } from "react-router-dom"
 import { doc, getDoc } from "firebase/firestore"
-import { db } from "../firebase/config"
+import { auth, db } from "../firebase/config"
 
 export const BlogPage = () => {
     const { id } = useParams() // get post id from  url
     const [post, setPost] = useState()
     const [date, setDate] = useState('')
-
-    console.log(post)
 
     useEffect(() => {
         async function getPost() {
@@ -24,6 +22,10 @@ export const BlogPage = () => {
         getPost()
     }, [id])
 
+    function handleEdit() {
+        console.log('edit')
+    }
+
     return (
         <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900 antialiased">
             <div className="flex justify-between px-4 mx-auto max-w-screen-lg">
@@ -38,11 +40,22 @@ export const BlogPage = () => {
                                 </div>
                             </div>
                         </address>
-                        <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">{post && post.title}</h1>
+                        <h1 className="flex justify-between mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+                            {post && post.title}
+                            {post && post.author.id === auth.currentUser.uid && (
+                                <button onClick={handleEdit} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                                    Edit
+                                    <svg className="ms-2 w-6 h-6 text-white dark:text-gray-800" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                                        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m14.304 4.844 2.852 2.852M7 7H4a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-4.5m2.409-9.91a2.017 2.017 0 0 1 0 2.853l-6.844 6.844L8 14l.713-3.565 6.844-6.844a2.015 2.015 0 0 1 2.852 0Z" />
+                                    </svg>
+                                </button>
+                            )}
+                        </h1>
                     </header>
-                    <section className="text-lg whitespace-pre-wrap">
+                    <section className="text-lg whitespace-pre-wrap text-justify">
                         <p>{post && post.content}</p>
                     </section>
+
                 </article>
             </div>
         </main>
