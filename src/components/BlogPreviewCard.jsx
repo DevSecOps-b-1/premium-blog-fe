d698f17fc06531184db9eb672e61cb9c228cfbb4
ariframaps <ariframa@student.ub.ac.id>
2024-05-08 08:59:37 +0700

    authors blog page interface

--- src/components/BlogPreviewCard.jsx
+++ src/components/BlogPreviewCard.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 
-export const BlogPreviewCard = ({ post }) => {
+export const BlogPreviewCard = ({ post, authors }) => {
     let { content } = post
 
     let postDate = post.createdAt.toDate().toString()
@@ -17,15 +17,28 @@ export const BlogPreviewCard = ({ post }) => {
             <p className="max-w-screen-md mb-3 font-normal text-gray-400 dark:text-gray-800">{postDate}</p>
             <p className="max-w-screen-md mb-7 font-normal text-gray-700 dark:text-gray-400">{contentPreview}...</p>
             <div className="flex w-full justify-between">
-                <Link to={`blog/${post.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                <Link to={`/blog/${post.id}`} className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                     Read more
                 </Link>
-                <span>
-                    <span className="text-gray-400">Author :</span>
-                    <Link to={`author/arif`} className="ms-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-400 hover:bg-gray-700 dark:bg-gray-600 dark:hover:bg-gray-700">
-                        {post.author.name}
-                    </Link>
-                </span>
+                {authors && (
+                    <div className="flex gap-3">
+                        <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-red-700 rounded-lg hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800">
+                            Delete
+                        </button>
+                        <button className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-700 rounded-lg hover:bg-gray-800 focus:ring-4 focus:outline-none focus:ring-gray-300 dark:bg-gray-600 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                            Edit
+                        </button>
+                    </div>
+                )}
+
+                {!authors && (
+                    <span>
+                        <span className="text-gray-400">Author :</span>
+                        <Link to={`author/arif`} className="ms-2 inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-gray-400 hover:bg-gray-700 dark:bg-gray-600 dark:hover:bg-gray-700">
+                            {post.author.name}
+                        </Link>
+                    </span>
+                )}
             </div>
         </div>
     )
--- src/pages/AuthorsBlogPage/AuthorsBlogPage.jsx
+++ src/pages/AuthorsBlogPage/AuthorsBlogPage.jsx
@@ -1,5 +1,44 @@
+import { collection, getDocs } from "firebase/firestore"
+import { useEffect, useState } from "react"
+import { BlogPreviewCard } from "../../components/BlogPreviewCard"
+import { db, auth } from "../../firebase/config"
+
 export const AuthorsBlogPage = () => {
+    const [blogPosts, setBlogPosts] = useState([])
+
+    console.log(auth)
+
+    useEffect(() => {
+        async function fetchPosts() {
+            let collRef = collection(db, 'blog-posts')
+            const data = await getDocs(collRef)
+            setBlogPosts(data.docs.map(doc => ({ ...doc.data(), id: doc.id })))
+            console.log('---')
+        }
+        fetchPosts()
+    }, [])
+
     return (
-        <div>AuthorsBlogPage</div>
+        <main className="py-10">
+            <div className="max-w-screen-xl m-auto">
+                <section className="h-fit p-5 rounded-lg shadow fixed justify-self-end">
+                    <div>
+                        <img className="mr-4 w-16 h-16 rounded-full" src={auth.currentUser.photoURL} alt="Jese Leos" />
+                        <div className="p-3">
+                            Name:
+                            <p rel="author" className="text-lg mb-3 ms-2 font-semibold text-gray-900 dark:text-white">{auth.currentUser.displayName}</p>
+                            Email:
+                            <p className="text-lg mb-3 ms-2 font-semibold text-gray-900 dark:text-white">{auth.currentUser.email}</p>
+                        </div>
+                    </div>
+                    <div className="p-3 text-lg font-semibold text-gray-900 dark:text-white border rounded-md shadow">Total post: 2</div>
+                </section>
+                <ul className="ms-72 flex flex-col gap-5">
+                    {blogPosts && blogPosts.map(post => (
+                        <BlogPreviewCard key={post.id} post={post} authors={true} />
+                    ))}
+                </ul>
+            </div>
+        </main>
     )
 }
