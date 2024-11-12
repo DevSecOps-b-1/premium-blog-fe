1ed048cd648eb3360a3861bc6851f4e679ed624e
ariframaps <ariframa@student.ub.ac.id>
2024-11-12 08:30:00 +0700

    fix premium button

--- src/components/BlogPreviewCard.jsx
+++ src/components/BlogPreviewCard.jsx
@@ -42,7 +42,11 @@ export const BlogPreviewCard = ({
           to={
             post.is_premium && !userStatus.is_premium ? `/` : `/blog/${post.id}`
           }
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className={`${
+            post.is_premium && !userStatus.is_premium
+              ? "cursor-not-allowed bg-gray-500"
+              : "bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          } inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white rounded-lg`}
         >
           {isHome ? "Read more" : "Edit"}
         </Link>
